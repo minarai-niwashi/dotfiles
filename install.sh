69d59bf7e0aa47e1603510c0aa655f3e0508e976
minarai-niwashi <58800220+minarai-niwashi@users.noreply.github.com>
2024-06-14 23:29:41 +0900

    oh-my-zshのプラグインを追加 (#3)
    * autosuggestionsプラグインを追加
    
    * syntax-highlightingプラグインを追加

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ sudo apt install -y zsh
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 # Copy custom zshrc if exists
 if [ -f ~/dotfiles/.zshrc ]; then
     cp ~/dotfiles/.zshrc ~/.zshrc
